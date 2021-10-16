d5f3526c7fdcbc39166a21aff9a8f02cc98471b8
energistix <50068022+energistix@users.noreply.github.com>
2021-10-16 12:22:11 +0200

    click event and some random other stuff

--- index.py
+++ index.py
@@ -3,27 +3,47 @@ from math import floor
 
 menu = Tk()
 
-def test(e):
+
+def click(e):
     print(e)
 
+
+class Grid():
+    def __init__(self, window: Tk, cell_size: int) -> None:
+        self.cells = []
+        self.cell_size = cell_size
+        self.window = window
+        self.canvas = Canvas(window, height=cell_size*3, width=cell_size*3)
+        self.canvas.pack()
+        for i in range(9):
+            self.cells.append(Cell(i, self.canvas))
+        for cell in self.cells:
+            self.canvas.create_rectangle(
+                cell.x*cell_size+2, cell.y*cell_size+2, cell.x*cell_size+cell_size-1, cell.y*cell_size+cell_size-1)
+            cell.draw()
+
+    def click_event(self, e: Event):
+        x = floor(e.x / self.cell_size)
+        y = floor(e.y / self.cell_size)
+        self.cells[y*3+x].click_event()
+
+
 class Cell():
-    def __init__(self, index:int, window: Tk) -> None:
+    def __init__(self, index: int, canvas: Canvas) -> None:
         self.value = ""
         self.index = index
         self.x = index % 3
         self.y = floor(index / 3)
-        self.window = window
-        self.label = Label(window, text=" ")
-        self.label.grid(row=self.y, column=self.x)
-        self.label.bind("<Button-1>", test)
+        self.canvas = canvas
 
-class Grid():
-    def __init__(self, window: Tk) -> None:
-        self.cells = []
-        self.window = window
-        for i in range(9):
-            self.cells.append(Cell(i, window))
+    def draw(self) -> None:
+        self.canvas.create_oval(self.x*50+5, self.y*50+5,
+                                self.x*50+45, self.y*50+45)
 
-Grid(menu)
-menu.bind("<Button-1>", test)
+    def click_event(self):
+        print(self.index)
+
+
+grid = Grid(menu, 50)
+menu.bind("<Button-1>", grid.click_event)
 menu.mainloop()
